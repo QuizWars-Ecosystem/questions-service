7882c505fa57042711db5477eb1c6a6345fb028a
Nofre <davidprogram417@gmail.com>
2025-04-17 23:27:07 +0200

    added new configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,6 +11,7 @@ type Config struct {
 	Postgres    PostgresConfig `envPrefix:"POSTGRES_"`
 	Redis       RedisConfig    `envPrefix:"REDIS_"`
 	StoreConfig StoreConfig    `envPrefix:"STORE_"`
+	JWT         JWTConfig      `envPrefix:"JWT_"`
 }
 
 type PostgresConfig struct {
@@ -25,4 +26,10 @@ type StoreConfig struct {
 	WarmUp        bool          `env:"WARM_UP"`
 	WarmUpAmount  int           `env:"WARM_UP_AMOUNT"`
 	WarmUpTimeout time.Duration `env:"WARM_UP_TIMEOUT"`
+}
+
+type JWTConfig struct {
+	Secret            string        `env:"SECRET"`
+	AccessExpiration  time.Duration `env:"ACCESS_EXPIRATION"`
+	RefreshExpiration time.Duration `env:"REFRESH_EXPIRATION"`
 }
